76a66b7787a913732b8796ab8cccaf27aa9ee8fb
Abhinavan Roy <abhinavan2772@gmail.com>
2020-01-15 17:48:22 +0530

    New Program

---
+++ Programs/String-Reverse.py
@@ -0,0 +1,12 @@
+#reversing the string
+def rev(x):
+      return x[::-1]
+
+#taking user input
+txt = input("Enter a sentence:")
+
+#functon called and result stored in new
+new = rev(txt)
+
+#displaying the result
+print(new)
