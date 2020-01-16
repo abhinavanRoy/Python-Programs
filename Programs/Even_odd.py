7ebfe7579c7e23ad535b86a6818aa09541f1b41b
Abhinavan Roy <abhinavan2772@gmail.com>
2020-01-16 10:40:13 +0530

    Added a new program

---
+++ Programs/Even_odd.py
@@ -0,0 +1,16 @@
+
+#checking even or odd
+def eocheck(i):
+    if(i%2) == 0:
+        print(i, " is an even number!")
+
+    else:
+        print(i," is an odd number!")
+
+
+
+#taking input from the user
+x = int(input("Enter any number:"))
+
+#function called
+eocheck(x)
